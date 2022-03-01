3643f04d9f8df910429283e0a504744ddf5f95f6
glinux99 <interlab21@yahoo.com>
2022-03-01 11:03:09 +0100

    interlab

--- humours/videosBlagues.php
+++ humours/videosBlagues.php
@@ -46,16 +46,23 @@
                         </div>
                     </div>
                     <div class="card col-lg-4">
-                        <img src="../i_img/inter1.jpg" alt="" class="card-img-top" height="200">
-                        <div class="card-img-overlay text-center d-flex pb-5">
-                            <div class="align-self-center mx-auto pb-4">
-                                <span class="bi-play-btn-fill bi--3xl text-danger"></span>
+                    <video width="100%" height="200" poster="initialimage.png" loop muted id="vidp1">
+                        <source src="i_videos/video1.mp4" type="video/mp4">
+                        <source src="i_videos/video1.webm" type="video/webm">
+                        <track src="subtitles_en.vtt" kind="subtitles" srclang="en" label="English">
+                        <a href="i_videos/video1.mp4">Télécharger la vidéo</a>
+                    </video>
+                        <form action="../i_videos/videos" method="post" id="formp1">
+                            <div class="card-img-overlay text-center d-flex pb-5" id="vid1">
+                                <div class="align-self-center mx-auto pb-5" id="play1">
+                                    <span class="bi-play-btn-fill bi--3xl text-danger"></span>
+                                </div>
                             </div>
-                        </div>
+                        </form>
                         <div class="card-body text-center p-0 h-50 overflow-hidden">
                             <h5>RDC en Ukraine</h5>
                             <p>Les fARDC en route vers l'Ukraine</p>
-                        </div>
+                        </div> 
                     </div>
                 </div>
                 <?php $x++;}?>
@@ -65,3 +72,20 @@
 </body>
 <?php include($footer);?>
 </html>
+<script>
+    $(document).ready(function(){
+      $("#vid1").hover(function(){
+        document.getElementById('vidp1').setAttribute("autoplay", '');
+      });
+      $("#vid1").mouseleave(function(){
+          $('#vidp1').trigger('pause');
+      });
+      $("#vid1").mouseenter(function(){
+          $('#vidp1').trigger('play');
+      });
+     $("#vid1").click(function(){
+          $('$formp1').submit()
+      });
+    });
+ </script>
+   
--- i_menu/header.php
+++ i_menu/header.php
@@ -3,7 +3,8 @@
         echo '<link rel="stylesheet" href="vendor/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="vendor/icons/font/bootstrap-icons.css">
         <link rel="stylesheet" href="css/css.css">
-        <script src="vendor/dist/js/bootstrap.bundle.min.js"></script>';
+        <script src="vendor/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="vendor/dist/js/jquery.min.js"></script>';
         $logo ="i_img/logo.png";
         $apropos ="i_services/apropos";
         $gospel = "i_gospel/gospel";
@@ -53,7 +54,8 @@
         echo '<link rel="stylesheet" href="'.$dir.'vendor/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="'.$dir.'vendor/icons/font/bootstrap-icons.css">
         <link rel="stylesheet" href="'.$dir.'css/css.css">
-        <script src="'.$dir.'vendor/dist/js/bootstrap.bundle.min.js"></script>';
+        <script src="'.$dir.'vendor/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="../vendor/dist/js/jquery.min.js"></script>';
     }
 ?>
 
